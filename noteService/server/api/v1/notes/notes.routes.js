d834251f24477cce3ee7741ce8ed19348034385a
Saha <404110@cognizant.com>
2019-03-11 19:38:10 +0530

    Notes service - ongoing

--- noteService/server/api/v1/notes/notes.routes.js
+++ noteService/server/api/v1/notes/notes.routes.js
@@ -1,13 +1,25 @@
 const router = require('express').Router();
 const controller = require('./notes.controller');
-const auth = require('../auth/auth');
+//const auth = require('../auth/auth');
 
-router.use(auth.isUserAuthenticated); //Notes will be accessible only if User is authenticated
+//router.use(auth.isUserAuthenticated); //Notes will be accessible only if User is authenticated
 
 router.get('/', controller.getNoteForUserID);
+
 router.post('/', controller.createNote);
 
 router.get('/:noteId', controller.getNoteForNoteID);
+
 router.put('/:noteId', controller.updateNotes);
+
+router.put('/share', controller.shareNote);
+
+router.delete('/delete', controller.deleteNotes);
+
+router.put('/addFavorites', controller.addNoteToFavourites);
+
+router.put('/addGroup', controller.addNoteToGroup);
+
+router.get('/isAllowed', controller.isUserAllowedForNote);
 
 module.exports = router;
