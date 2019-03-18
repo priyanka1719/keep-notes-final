1e59dacac7d0897f97b2dca158cf9b5f2056e3d5
Saha <404110@cognizant.com>
2019-03-18 19:21:39 +0530

    Updates

--- apigateway/server/bin/www.js
+++ apigateway/server/bin/www.js
@@ -5,12 +5,12 @@ const log = require('../logging');
 
 const port = config.WWW_PORT;
 
-// app.listen(port, () => {
-//   //console.log(`API Gateway App is listening to ${port}`);
-// })
-
-const server = require('http').createServer(app);
-
-server.listen(port, () => {
+app.listen(port, () => {
   log.info(`Server is running on port ${port}`);
-});
+})
+
+// const server = require('http').createServer(app);
+
+// server.listen(port, () => {
+//   log.info(`Server is running on port ${port}`);
+// });
